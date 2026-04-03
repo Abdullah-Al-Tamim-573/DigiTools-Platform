d51fc180e7742f17357255de5fff0d7cc54246d6
Abdullah-Al-Tamim-573 <abdullahaltamim573@gmail.com>
2026-04-03 22:26:47 +0600

    create a product and every looping i add this card in product component and disstractaring it and show it in the product component

---
+++ src/Components/Premium Digital Tools/Products and Cards/Product.jsx
@@ -0,0 +1,76 @@
+
+
+const Product = ({ product }) => {
+  console.log(product);
+  let {id, name, description, price, icon, alt, period, tag, features} = product
+  return (
+    <>
+      <div className="card w-[85%] justify-items-center mx-auto md:w-75 lg:w-96 bg-base-100 shadow-sm relative border ">
+        <div className="card-body">
+          <span className="badge badge-xs badge-warning absolute top-3 right-3 !text-[#BB4D00] bg-[#fef3c6] text-[14px] font-medium px-4 py-3 rounded-4xl">
+            {tag}
+          </span>
+          <span className="border p-2 rounded-full w-fit">
+            <img src={icon} alt={alt} />
+          </span>
+          <div className="flex flex-col gap-2 mt-2">
+            <h2 className="text-2xl font-bold">{name}</h2>
+            <p>
+              {description}
+            </p>
+            <span className="text-xl">${price}/mo</span>
+          </div>
+          <ul className="mt-6 flex flex-col gap-2 text-xs">
+            {
+               features.map(feture =>  <>
+                      <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>{feture}</span>
+            </li>
+               </>) 
+              
+            }
+            <li>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="size-4 me-2 inline-block text-success"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 13l4 4L19 7"
+                />
+              </svg>
+              <span>High-resolution image generation</span>
+            </li>
+            
+          </ul>
+          <div className="mt-6">
+            <button className="btn !text-white linier-Blue btn-block">
+              Buy Now
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Product;
--- src/Components/Premium Digital Tools/Products and Cards/Products.jsx
+++ src/Components/Premium Digital Tools/Products and Cards/Products.jsx
@@ -1,14 +1,19 @@
 import { use } from "react";
+// import cardImg from '../../../assets/products/social-media.png'
+import Product from "./product";
 
+const Products = ({ productCardDatasPromise }) => {
+  let productCardDatas = use(productCardDatasPromise);
 
-const Products = ({productCardDatasPromise}) => {
-    let productCardDatas = use(productCardDatasPromise);
-    console.log(productCardDatas)
-    return (
-        <>
-            
-        </>
-    );
+  return (
+    <>
+      <section className="grid gap-5  mx-auto grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {productCardDatas.map((product, i) => (
+          <Product key={i} product={product}></Product>
+        ))}
+      </section>
+    </>
+  );
 };
 
 export default Products;
