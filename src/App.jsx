a3bc6255a20d7fe8bf9777962be38a329e391c41
Abdullah-Al-Tamim-573 <abdullahaltamim573@gmail.com>
2026-04-04 15:51:59 +0600

    remove duplicate by clicking in app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-
-
 import { useState } from "react";
 import "./App.css";
 import Banner from "./Components/Banner/Banner";
@@ -7,38 +5,40 @@ import Navbar from "./Components/Navbar/Navbar";
 import PremiumDigitalTools from "./Components/Premium Digital Tools/PremiumDigitalTools";
 import StatusBar from "./Components/StatusBar/StatusBar";
 
-
-
-
 function App() {
-
   let [productCardData, setProductCardData] = useState([]);
 
   let handleProductCardData = (cardObj) => {
-       setProductCardData([...productCardData, cardObj])
+
+    let matchesData = productCardData.filter(cardData => cardData.id === cardObj.id)
+
+     if (matchesData.length === 0) {
+    setProductCardData([...productCardData, cardObj]);
+  }   
   }
 
- 
-  
+  console.log(productCardData)
+   
+
+  // console.log(productCardData);
 
   return (
     <>
-        
-        <section className="lg:max-w-[80%] mx-auto">
-                  <Navbar productCardData={productCardData}></Navbar>
-                  <Banner></Banner>
-        </section>
+      <section className="lg:max-w-[80%] mx-auto">
+        <Navbar productCardData={productCardData}></Navbar>
+        <Banner></Banner>
+      </section>
 
-        <section className="linier-Blue py-5">
-                   <StatusBar></StatusBar>
-        </section>
-        
-        <section className="lg:max-w-[80%] mx-auto mt-3 md:mt-12 lg:mt-17">
-              
-                    <PremiumDigitalTools handleProductCardData={handleProductCardData} productCardData={productCardData}></PremiumDigitalTools>
-              
-        </section>
-    
+      <section className="linier-Blue py-5">
+        <StatusBar></StatusBar>
+      </section>
+
+      <section className="lg:max-w-[80%] mx-auto mt-3 md:mt-12 lg:mt-17">
+        <PremiumDigitalTools
+          handleProductCardData={handleProductCardData}
+          productCardData={productCardData}
+        ></PremiumDigitalTools>
+      </section>
     </>
   );
 }
